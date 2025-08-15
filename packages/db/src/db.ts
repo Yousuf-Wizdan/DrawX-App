60031b9ef8ddaf87de9ee4a5d0e489fd1a40e68e
Yousuf-Wizdan <mdyousufwizdan@gmail.com>
2025-08-15 19:37:04 +0530

    Added DB

---
+++ packages/db/src/db.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "./generated/prisma";
+
+declare global {
+  var db: PrismaClient | undefined;
+}
+
+export const db =
+  global.db ||
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  global.db = db;
+}
