0ccc7c26da2602c12596c91638b5c955d3d29b90
Yousuf-Wizdan <mdyousufwizdan@gmail.com>
2025-08-15 18:40:26 +0530

    common pkg fix

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,11 +2,17 @@ import express from 'express';
 import jwt from "jsonwebtoken"
 import { JWT_SECRET } from '@repo/backend-common/config';
 import middleware from './middleware';
+import {CreateUserSchema , SignInSchema , CreateRoomSchema} from '@repo/common/types'
 
 const app = express();
 
 app.post('/signup' , (req,res) => {
-    const {username , password} = req.body;
+    const data = CreateUserSchema.safeParse(req.body)
+    if(!data.success){
+        return res.json({
+            message: 'Incorrect Inputs'
+        })
+    }
     
     // db call
 
@@ -16,6 +22,13 @@ app.post('/signup' , (req,res) => {
 })
 
 app.post('/signin' , (req,res) => {
+    const data = SignInSchema.safeParse(req.body)
+    if(!data.success){
+        return res.json({
+            message: 'Incorrect Inputs'
+        })
+    }
+
     const userId = 1;
     const token = jwt.sign({
         userId
@@ -27,9 +40,14 @@ app.post('/signin' , (req,res) => {
 })
 
 app.post('/room' , middleware , (req,res) => {
+    const data = CreateRoomSchema.safeParse(req.body)
+    if(!data.success){
+        return res.json({
+            message: 'Incorrect Inputs'
+        })
+    }
 
     // db call
-
     res.json({
         roomId: 123
     })
